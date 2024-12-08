a5fa74086af8af0b2577e98c2a91ce096f6bb4bb
PedroOttavio <pedroottavioss@gmail.com>
2024-12-08 01:24:47 -0300

    alteracoes no login

--- view/login.php
+++ view/login.php
@@ -36,12 +36,12 @@
             <input type="password" class="form-control" id="password" placeholder="Digite sua senha">
           </div>
           <div class="mb-3">
-              <button type="submit" class="btn btn-secundary w-100 btn-custom-border">Entrar</button>
+            <button type="submit" class="btn btn-secundary w-100 btn-custom-border">Entrar</button>
 
-            
+
           </div>
           <!-- <button type="submit" class="btn btn-secundary btn-custom-border "> <a class="no-underline text-black" href="./cadastroUser.php">NOVO por aqui???Crie sua conta agora mesmo clicando aqui</a></button> -->
-          <a href="./cadastroUser.php" class="float-start no-underline text-black">Criar conta</a> 
+          <a href="./cadastroUser.php" class="float-start no-underline text-black">Criar conta</a>
         </form>
       </div>
     </div>
