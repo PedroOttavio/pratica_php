e49661a66934b78cd376c0a47de58c303f6e16d1
PedroOttavio <pedroottavioss@gmail.com>
2024-12-11 11:57:50 -0300

    m

--- view/login.php
+++ view/login.php
@@ -1,17 +1,10 @@
-<?php
-
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>Pagina Login</title>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -25,28 +18,32 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-4">
-        <h2 class="text-center mt-5">Login</h2>
-        <form>
+        <h2 class="text-center mt-5 mb-5">Entrar</h2>
+        <form method="post" action="../parte-de-controle/controleUsuario.php">
           <div class="mb-3">
-            <label for="email" class="form-label">E-mail</label>
-            <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail">
+            <label for="LoginUsuario" class="form-label">Login</label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="LoginUsuario" name="LoginUsuario" placeholder="Nome de usuário" required>
+            </div>
           </div>
           <div class="mb-3">
-            <label for="password" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="password" placeholder="Digite sua senha">
+            <label for="SenhaUsuario" class="form-label">Senha</label>
+            <div class="input-group">
+              <input type="password" class="form-control" id="SenhaUsuario" name="SenhaUsuario" placeholder="Senha" required>
+            </div>
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="rememberMe">
+            <label class="form-check-label" for="rememberMe">Lembrar de mim</label>
           </div>
           <div class="mb-3">
-            <button type="submit" class="btn btn-secundary w-100 btn-custom-border">Entrar</button>
-
-
+            <button type="submit" name="opcao" value="Entrar" class="btn btn-secundary w-100 btn-custom-border">Entrar</button>
           </div>
-          <!-- <button type="submit" class="btn btn-secundary btn-custom-border "> <a class="no-underline text-black" href="./cadastroUser.php">NOVO por aqui???Crie sua conta agora mesmo clicando aqui</a></button> -->
-          <a href="./cadastroUser.php" class="float-start no-underline text-black">Criar conta</a>
+          <a href="cadastrarUsuario.php" class="float-start no-underline text-black">Criar conta</a>
         </form>
       </div>
     </div>
   </div>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
