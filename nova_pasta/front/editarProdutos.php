26db3299b06ae01aabdf2db5b6f1083807c3ae89
PedroOttavio <pedroottavioss@gmail.com>
2024-12-11 11:49:25 -0300

    teste editarProdutos

--- nova_pasta/front/editarProdutos.php
+++ nova_pasta/front/editarProdutos.php
@@ -15,7 +15,6 @@ foreach ($resultados as $linha);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    
     <style>
         body {
             font-family: 'Roboto', sans-serif;
@@ -56,7 +55,6 @@ foreach ($resultados as $linha);
     <div class="container mt-5">
         <h2><strong>Editar Produto</strong></h2>
 
-        <!-- Formulário de Edição -->
         <form method="post" action="../control/controleProdutos.php">
             <div class="mb-3 col-md-9">
                 <label for="NomeProduto" class="form-label mt-4">Nome do Produto</label>
@@ -77,7 +75,6 @@ foreach ($resultados as $linha);
         </form>
     </div>
 
-    <!-- Bootstrap JS (opcional) -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
