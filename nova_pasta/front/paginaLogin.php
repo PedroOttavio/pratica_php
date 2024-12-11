74523d6eaed9b6637c6b924dde4ab00f3597653a
PedroOttavio <pedroottavioss@gmail.com>
2024-12-11 11:31:48 -0300

    teste01 login

--- nova_pasta/front/paginaLogin.php
+++ nova_pasta/front/paginaLogin.php
@@ -11,32 +11,30 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="../estilo/estilo.css">
+  <link rel="stylesheet" href="../../estilo/estilo.css">
 </head>
 
 <body>
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-4">
-        <h2 class="text-center mt-5">Logar</h2>
-        <form method="post" action="../control/controleUsuario.php">
+        <h2 class="text-center mt-5">Entrar</h2>
+        <form method="post" action="../parte-de-controle/controleUsuario.php">
           <div class="mb-3">
             <label for="LoginUsuario" class="form-label">Login</label>
             <div class="input-group">
-              <span class="input-group-text"><i class="fas fa-user"></i></span>
-              <input type="text" class="form-control" id="LoginUsuario" name="LoginUsuario" placeholder="Digite seu login" required>
+              <input type="text" class="form-control" id="LoginUsuario" name="LoginUsuario" placeholder="login" required>
             </div>
           </div>
           <div class="mb-3">
             <label for="SenhaUsuario" class="form-label">Senha</label>
             <div class="input-group">
-              <span class="input-group-text"><i class="fas fa-key"></i></span>
               <input type="password" class="form-control" id="SenhaUsuario" name="SenhaUsuario" placeholder="Digite sua senha" required>
             </div>
           </div>
           <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="rememberMe">
-            <label class="form-check-label" for="rememberMe">Lembre de mim</label>
+            <label class="form-check-label" for="rememberMe">Lembrar de mim</label>
           </div>
           <div class="mb-3">
             <button type="submit" name="opcao" value="Entrar" class="btn btn-secundary w-100 btn-custom-border">Entrar</button>
@@ -46,7 +44,6 @@
       </div>
     </div>
   </div>
-  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
